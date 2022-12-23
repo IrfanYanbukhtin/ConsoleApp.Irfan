0976c4862e465cccea2664ae5ceea8faa61a43e3
irfan <Asery_valha@DESKTOP-707VBL0>
2022-12-23 15:56:18 +0400

    Добавьте файлы проекта.

---
+++ Consoleapp.Irfan/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp.Irfan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a,b;
+            a = 2.5;
+            b = 3.41;
+
+            Console.WriteLine(a + b);
+            
+                
+        }
+    }
+}
